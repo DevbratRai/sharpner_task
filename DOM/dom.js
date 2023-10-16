1d3c64a35868ef0db8d760b17da1a2f8db41bd04
Dev <raidb2002@gmail.com>
2023-10-16 11:25:02 +0530

    changes done in dom.html and dom.js

--- DOM/dom.js
+++ DOM/dom.js
@@ -9,8 +9,20 @@ console.log(addItem);
 addItem[0].style.fontWeight = "bold";
 addItem[0].style.color = "green";
 
-const items = document.getElementsByClassName("list-group-item");
-items[2].style.background = "green";
-for(let i=0;i<items.length;i++){
-    items[i].style.fontWeight = "bold";
+// const items = document.getElementsByClassName("list-group-item");
+// items[2].style.background = "green";
+// for(let i=0;i<items.length;i++){
+//     items[i].style.fontWeight = "bold";
+//     items[i].style.background = "gray";
+// }
+
+//get element by tag name
+
+const li = document.getElementsByTagName("li");
+li[2].style.background = "green";
+for(let i=0;i<li.length;i++){
+    li[i].style.fontWeight = "bold";
+       li[i].style.background = "gray";
 }
+
+

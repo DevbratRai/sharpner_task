54829c21c3b1cc71e50fdfd5293f360430b4efa4
Dev <raidb2002@gmail.com>
2023-10-16 12:02:47 +0530

    changes done by using selector

--- DOM/dom.js
+++ DOM/dom.js
@@ -26,3 +26,25 @@ for(let i=0;i<li.length;i++){
 }
 
 
+//querySelector
+
+// const secondItem = document.querySelector(".list-group-item:nth-child(2)");
+// secondItem.style.background = "green";
+
+// const thirdItem = document.querySelector(".list-group-item:nth-child(3)");
+// thirdItem.style.visibility = "hidden";
+
+
+//querySelectorAll
+
+const items = document.querySelectorAll(".list-group-item");
+console.log(items);
+items[1].style.color = "green";
+
+const odd = document.querySelectorAll("li:nth-child(odd)");
+for(let i=0;i<odd.length;i++){
+    odd[i].style.background = "green";
+}
+
+
+

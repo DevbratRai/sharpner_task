71371d0475ce69d3447bdeadd78ea68d69997002
Dev <raidb2002@gmail.com>
2023-10-24 12:36:18 +0530

    create-Post

--- main.js
+++ main.js
@@ -31,24 +31,73 @@
 // console.log("person4:shows ticket");
 // console.log("person5:shows ticket");
 
-console.log("Person1: Shows Tickt");
-console.log("Person2: Shows Tickt");
+// console.log("Person1: Shows Tickt");
+// console.log("Person2: Shows Tickt");
 
-const preMovie = async ()=>{
-    const promiseWifeBringingTicks = new Promise((resolve,reject)=>{
-            setTimeout(()=>{
-                resolve("ticket");
-            },3000)
-    })
+// const preMovie = async ()=>{
+//     const promiseWifeBringingTicks = new Promise((resolve,reject)=>{
+//             setTimeout(()=>{
+//                 resolve("ticket");
+//             },3000)
+//     })
     
-    const getPopcorn = new Promise((resolve,reject)=> resolve(`popcorn`));
-    const addButter = new Promise((resolve,reject)=> resolve(`butter`));
-    const getColdDrinks = new Promise((resolve,reject)=> resolve(`coldDrinks`));
+//     const getPopcorn = new Promise((resolve,reject)=> resolve(`popcorn`));
+//     const addButter = new Promise((resolve,reject)=> resolve(`butter`));
+//     const getColdDrinks = new Promise((resolve,reject)=> resolve(`coldDrinks`));
 
-    let ticket = await promiseWifeBringingTicks;
-    let [popcorn,Butter,coldDrinks] = await Promise.all([getPopcorn,addButter,getColdDrinks]);
-    console.log(`${popcorn},${Butter}, ${coldDrinks}`);
-    return ticket
+//     let ticket = await promiseWifeBringingTicks;
+//     let [popcorn,Butter,coldDrinks] = await Promise.all([getPopcorn,addButter,getColdDrinks]);
+//     console.log(`${popcorn},${Butter}, ${coldDrinks}`);
+//     return ticket
 
+// }
+// preMovie().then((m)=> console.log(`person3: shows ${m}`));
+
+const posts = [{title: 'POST1'},{title: 'POST2'}];
+
+
+
+const allPost = async ()=>{
+    function printPost() {
+        return new Promise((resolve,reject)=>{
+            setTimeout(()=>{
+            resolve(posts)
+            },3000)
+        })
+        
+    }
+
+    function create3rdPost() {
+        return new Promise( (resolve, reject) => {
+            setTimeout( () => {
+                posts.push({title: 'POST3'});
+                resolve()
+            }, 2000)
+        }) 
+    }
+    
+    function create4thPost() {
+        return new Promise( (resolve, reject) => {
+            setTimeout( () => {
+                posts.push({title: 'POST4'});
+                resolve();
+            }, 2000)
+        }) 
+    }
+    
+    function deletePost(){
+        return new Promise((resolve,reject)=>{
+        setTimeout(()=>{
+            posts.pop();
+            resolve();
+    
+        },2000)
+        
+    })
+    }
+    let allValue = await printPost();
+    let [third,fourth,del] = await Promise.all([create3rdPost(),create4thPost(),deletePost()]);
+    return allValue;
 }
-preMovie().then((m)=> console.log(`person3: shows ${m}`));
+allPost().then((data)=> console.log(data));
+

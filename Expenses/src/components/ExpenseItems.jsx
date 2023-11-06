add7dbc70bbe4d52513b982c7891fb3786c959b0
Dev <raidb2002@gmail.com>
2023-11-06 19:26:24 +0530

    Expense first start

---
+++ Expenses/src/components/ExpenseItems.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+function ExpenseItems() {
+  return (
+    <div>
+      <div>
+        <h1>Expense Items</h1>
+      </div>
+      <h3>Food Rs 10</h3>
+      <h3> Petrol Rs 100</h3>
+      <h3>Movies Rs 200</h3>
+    </div>
+  );
+}
+
+export default ExpenseItems;

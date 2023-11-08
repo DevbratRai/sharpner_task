5c16cad412ef627bb93adcbd60890caca87d73b5
Dev <raidb2002@gmail.com>
2023-11-08 20:07:43 +0530

    getting the input value in console

--- Expenses/src/components/NewExpenses/ExpenseForm.jsx
+++ Expenses/src/components/NewExpenses/ExpenseForm.jsx
@@ -2,20 +2,42 @@ import React from "react";
 import "./ExpenseForm.css";
 
 function ExpenseForm() {
+  const titleChangeHandler = (e) => {
+    console.log(e.target.value);
+  };
+
+  const amountChangeHandler = (e) => {
+    console.log(e.target.value);
+  };
+
+  const dateChangeHandler = (e) => {
+    console.log(e.target.value);
+  };
+
   return (
     <form>
       <div className="new-expense_controls">
         <div className="new-expense_control">
           <label htmlFor="">Title</label>
-          <input type="text" />
+          <input type="text" onChange={titleChangeHandler} />
         </div>
         <div className="new-expense_control">
           <label htmlFor="">Amount</label>
-          <input type="number" min="0.01" step="0.01" />
-        </div>{" "}
+          <input
+            type="number"
+            min="0.01"
+            step="0.01"
+            onChange={amountChangeHandler}
+          />
+        </div>
         <div className="new-expense_control">
           <label htmlFor="">Date</label>
-          <input type="date" min="2019-01-01" max="2023-12-31" />
+          <input
+            type="date"
+            min="2019-01-01"
+            max="2023-12-31"
+            onChange={dateChangeHandler}
+          />
         </div>
       </div>
       <div className=".new-expense_actions">

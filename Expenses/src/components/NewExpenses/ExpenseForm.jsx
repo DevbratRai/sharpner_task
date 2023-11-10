f8ff513e8413a739ba762a04276df6750c5ab423
Dev <raidb2002@gmail.com>
2023-11-10 18:18:00 +0530

    submitting the forms

--- Expenses/src/components/NewExpenses/ExpenseForm.jsx
+++ Expenses/src/components/NewExpenses/ExpenseForm.jsx
@@ -6,29 +6,61 @@ function ExpenseForm() {
   const [enteredAmount, setEnteredAmount] = useState("");
   const [enteredDate, setEnteredDate] = useState("");
 
+  //   const [userInput, setUserInput] = useState({
+  //     enteredtitle: "",
+  //     enteredAmount: "",
+  //     enteredDate: "",
+  //   });
+
   const titleChangeHandler = (e) => {
     setEnteredTitle(e.target.value);
+    // setUserInput((prevState) => {
+    //   return { ...prevState, enteredtitle: e.target.value };
+    // });
   };
 
   const amountChangeHandler = (e) => {
     setEnteredAmount(e.target.value);
+    // setUserInput((prevState) => {
+    //   return { ...prevState, enteredAmount: e.target.value };
+    // });
   };
 
   const dateChangeHandler = (e) => {
     setEnteredDate(e.target.value);
+    // setUserInput((prevState) => {
+    //   return { ...prevState, enteredDate: e.target.value };
+    // });
+  };
+  const submitHandler = (event) => {
+    event.preventDefault();
+    const expenseData = {
+      title: enteredtitle,
+      amount: enteredAmount,
+      date: new Date(enteredDate),
+    };
+    console.log(expenseData);
+    setEnteredTitle("");
+    setEnteredAmount("");
+    setEnteredDate("");
   };
 
   return (
-    <form>
+    <form onSubmit={submitHandler}>
       <div className="new-expense_controls">
         <div className="new-expense_control">
           <label htmlFor="">Title</label>
-          <input type="text" onChange={titleChangeHandler} />
+          <input
+            type="text"
+            value={enteredtitle}
+            onChange={titleChangeHandler}
+          />
         </div>
         <div className="new-expense_control">
           <label htmlFor="">Amount</label>
           <input
             type="number"
+            value={enteredAmount}
             min="0.01"
             step="0.01"
             onChange={amountChangeHandler}
@@ -38,6 +70,7 @@ function ExpenseForm() {
           <label htmlFor="">Date</label>
           <input
             type="date"
+            value={enteredDate}
             min="2019-01-01"
             max="2023-12-31"
             onChange={dateChangeHandler}

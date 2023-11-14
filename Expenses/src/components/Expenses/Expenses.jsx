bd35d89d4f87648f91baf0a7c1ba1b350ae9c81b
Dev <raidb2002@gmail.com>
2023-11-14 14:29:22 +0530

    Chart App

--- Expenses/src/components/Expenses/Expenses.jsx
+++ Expenses/src/components/Expenses/Expenses.jsx
@@ -4,6 +4,7 @@ import "./ExpenseItems.css";
 import "./Expenses.css";
 import ExpensesFilter from "./ExpensesFilter";
 import ExpensesList from "./ExpensesList";
+import ExpensesChart from "./ExpensesChart";
 
 const Expenses = (props) => {
   const [filteredYear, setFilteredYear] = useState("2023");
@@ -22,6 +23,7 @@ const Expenses = (props) => {
         selected={filteredYear}
         onChangeFilter={filterChangeHandler}
       />
+      <ExpensesChart expenses={filteredExpenses} />
       <ExpensesList item={filteredExpenses} />
     </Card>
   );
